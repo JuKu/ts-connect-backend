db8594cde81b7faedcef06a89bf19c0484b38401
Justin Kuenzel <kuenzel.justin@t-online.de>
2022-01-28 02:42:01 +0100

    Add logger

--- src/shared/system/logger/logger.ts
+++ src/shared/system/logger/logger.ts
@@ -8,6 +8,12 @@ const levels = {
   debug: 4,
 }
 
+const level = () => {
+  const env = process.env.NODE_ENV || 'development'
+  const isDevelopment = env === 'development'
+  return isDevelopment ? 'debug' : 'info'
+}
+
 const colors = {
   error: 'red',
   warn: 'yellow',
@@ -34,7 +40,7 @@ export class Logger {
         const { createLogger, format, transports } = require('winston');
 
         const logger = createLogger({
-            level: 'debug',
+            level: level(),//'info',
             format: format.combine(
               format.timestamp({
                 format: "YYYY-MM-DD'T'HH:mm:ss.SSSZ"
@@ -46,13 +52,34 @@ export class Logger {
               new transports.File({
                 filename: 'logs/error.log',
                 level: 'warn',
-              })
+              }),
+              transports.File({ filename: 'logs/all.log' })
             ]
           });
     }
 
     public static logger() {
         return this.LOGGER;
+    }
+
+    public static debug(msg: String) {
+      this.logger().debug(msg);
+    }
+
+    public static info(msg: String) {
+      this.logger().info(msg);
+    }
+
+    public static http(msg: String) {
+      this.logger().http(msg);
+    }
+
+    public static warn(msg: String) {
+      this.logger().warn(msg);
+    }
+
+    public static error(msg: String) {
+      this.logger().error(msg);
     }
 
 }
--- src/web-api/app.ts
+++ src/web-api/app.ts
@@ -1,8 +1,10 @@
-'use strict'
+"use strict";
 
 import express from "express";
 import {Logger} from "../shared/system/logger/logger";
-var fs = require('fs');
+import fs from "fs";
+import { HttpApiServer } from "../shared/system/server/apiserver";
+import { Request, Response } from "express";
 
 /**
  * this is the main file for the web-api application.
@@ -19,37 +21,41 @@ const PORT = process.env.PORT || 3000;
 const ROOT_PATH = __dirname;
 console.info(`ROOT_PATH: ${ROOT_PATH}`);
 
-const SERVER_VERSION = JSON.parse(fs.readFileSync(ROOT_PATH + '/../../package.json', 'utf8')).version;
+const SERVER_VERSION = JSON.parse(fs.readFileSync(
+    ROOT_PATH + "/../../package.json", "utf8"),
+).version;
 
-//initialize the logger
+// initialize the logger
 Logger.init();
 
 // create a new express application
 const app = express();
 
-//register express nmiddleware
+// register express nmiddleware
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded());
 
+const apiServer: HttpApiServer = new HttpApiServer(app);
+
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
+apiServer.get( "/", ( req: Request, res: Response ) => {
   res.send( "This is the public API of the ts-connect-app." );
 } );
 
 // TODO: add api endpoints here
 app.get("/api/version", (req, res) => {
   // get the current version from package.json
-  //import {version} from "../../package.json";
-  //const pjson = require("./package.json");
+  // import {version} from "../../package.json";
+  // const pjson = require("./package.json");
 
   const version = {
-    "backend-version": SERVER_VERSION,//pjson.version,
+    "backend-version": SERVER_VERSION, // pjson.version,
   };
   res.json(version);
 });
 
 // start the Express server
 app.listen( PORT, () => {
-  console.log( `server started at http://${ HOST }:${ PORT }` );
+  Logger.info( `server started at http://${ HOST }:${ PORT }` );
 } );
