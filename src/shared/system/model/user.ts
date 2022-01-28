8afb80805b9aea3041bf4103880e58499a183baf
Justin Kuenzel <kuenzel.justin@t-online.de>
2022-01-28 03:32:53 +0100

    Add user model

---
+++ src/shared/system/model/user.ts
@@ -0,0 +1,21 @@
+import { model, Schema, Model, Document } from 'mongoose';
+
+interface IUser extends Document {
+  username: string;
+  password: string;
+  email: string;
+  prename: string;
+  lastName: string;
+}
+
+const UserSchema: Schema = new Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true }
+});
+
+const User: Model<IUser> = model('User', UserSchema);
+
+module.exports = User;
