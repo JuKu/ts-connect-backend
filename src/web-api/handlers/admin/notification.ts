0341cb93d4f28e312f1180afa45122ca698557da
Justin Kuenzel <kuenzel.justin@t-online.de>
2022-02-01 04:14:14 +0100

    Add api endpoint to send topic notifications

--- src/web-api/handlers/admin/notification.ts
+++ src/web-api/handlers/admin/notification.ts
@@ -1,5 +1,7 @@
 import {Request, Response} from "express";
 import {hasRole} from "../../../shared/system/middleware/check-role";
+// eslint-disable-next-line max-len
+import {FirebaseAdminSDK} from "../../../shared/system/firebase/firebase-admin-sdk";
 
 const {check} = require("express-validator");
 
@@ -10,10 +12,11 @@ const {check} = require("express-validator");
  * @author Justin Kuenzel
  */
 module.exports = async () => {
-  app.post("/api/login", [
-    check("username").isLength({min: 3}).trim().escape(),
+  app.post("/api/admin/topic-notification", [
+    check("title").isLength({min: 3}).trim().escape(),
     // check('email').isEmail().normalizeEmail(),
-    check("password").isLength({min: 3}).trim().escape(),
+    check("message").isLength({min: 3}).trim().escape(),
+    check("targetTopic").isLength({min: 3}).trim().escape(),
     // eslint-disable-next-line max-len
   ], authCheck, hasRole(["super-admin", "super-developer"]), async (req: Request, res: Response) => {
     // @ts-ignore
@@ -21,9 +24,14 @@ module.exports = async () => {
     // @ts-ignore
     const username: String = req.user.username;
 
-    const topicName = "all";
+    const title = req.body.title;
+    const message = req.body.message;
+    const topicName = req.body.targetTopic;
 
     logger.info("create notification to topic '" + topicName + "'",
-        {"authorID": userid, "author": username});
+        {"authorID": userid, "author": username, "title": title,
+          "message": message});
+
+    FirebaseAdminSDK.sendTopicNotification(topicName, title, message);
   });
 };
