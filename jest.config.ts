89ff8c24a808bc0ad5f84646b96bb96ed42ab443
Justin Kuenzel <kuenzel.justin@t-online.de>
2022-02-12 05:05:32 +0100

    Add jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,5 @@
 /*
- * For a detailed explanation regarding each configuration property and
- * type check, visit:
+ * For a detailed explanation regarding each configuration property and type check, visit:
  * https://jestjs.io/docs/configuration
  */
 
@@ -27,12 +26,12 @@ export default {
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: [
-    "\\\\node_modules\\\\",
-  ],
+  // coveragePathIgnorePatterns: [
+  //   "\\\\node_modules\\\\"
+  // ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
+  // coverageProvider: "babel",
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
@@ -152,11 +151,10 @@ export default {
   //   "**/?(*.)+(spec|test).[tj]s?(x)"
   // ],
 
-  // An array of regexp pattern strings that are matched against all test paths,
-  // matched tests are skipped
-  testPathIgnorePatterns: [
-    "\\\\node_modules\\\\",
-  ],
+  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+  // testPathIgnorePatterns: [
+  //   "\\\\node_modules\\\\"
+  // ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
