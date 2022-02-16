377c9af85b266ada3f5e2b5cce9560329f7a2d96
Justin Kuenzel <kuenzel.justin@t-online.de>
2022-02-16 22:33:47 +0100

    Change circle ci configuration, add sonarqube

--- jest.config.ts
+++ jest.config.ts
@@ -120,9 +120,9 @@ export default {
   // rootDir: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+  roots: [
+    "src"
+  ],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
@@ -163,7 +163,7 @@ export default {
   // testRegex: [],
 
   // This option allows the use of a custom results processor
-  //testResultsProcessor: "jest-sonar-reporter",
+  testResultsProcessor: "jest-sonar-reporter",
 
   // This option allows use of a custom test runner
   // testRunner: "jest-circus/runner",
